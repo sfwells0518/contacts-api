35f57ae79333a82bb530d3b46b9be9716e09b88f
Sam Wells <sfwells0518@gmail.com>
2023-04-05 13:34:25 -0500

    add address format validation for latitude longitude

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+  validates :address, format: { with: /\A-?([1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?),\s?-?((1?[0-7]?[0-9](?:\.\d{1,18})?|180(?:\.0{1,18})?))\z/, message: ",Address must be in valid latitude, longitude format" }
 end
